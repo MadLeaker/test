b4160254289ca364f6093eec0dbce917dd492323
MadLeaker <43907203+MadLeaker@users.noreply.github.com>
2020-10-15 19:03:43 +0300

    Update date.ts

--- api/date.ts
+++ api/date.ts
@@ -2,5 +2,5 @@ import { NowRequest, NowResponse } from '@now/node';
 
 export default (_req: NowRequest, res: NowResponse) => {
   const date = new Date().toString();
-  res.status(200).send(date);
+  res.status(200).send({date});
 };
